b149c04f308a0b34ae719b87a7dac0ef6fec86d8
bryanchun <chun.hiu.sang@gmail.com>
2021-06-01 21:16:53 +0800

    Initial scaffolding

---
+++ lib/parsers.ex
@@ -0,0 +1,18 @@
+defmodule Parsers do
+  @moduledoc """
+  Documentation for `Parsers`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Parsers.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
