eae6c84a1244f6f6edada009e9fe6b9c785d115c
bryanchun <chun.hiu.sang@gmail.com>
2021-06-02 00:00:22 +0800

    Refactor prop test for code reuse

--- test/parsers/iso_datetime_prop_test.exs
+++ test/parsers/iso_datetime_prop_test.exs
@@ -12,13 +12,31 @@ defmodule Parsers.IsoDatetimePropTest do
     |> String.pad_leading(2, "0")
   end
 
-  property "parses a datetime of all valid time" do
-    forall [year, month, day, hour, minute, second] <- [
-      integer(1000, 9999), integer(1, 12), integer(1, 31), integer(0, 23), integer(0, 59), integer(0, 59)
+  defp naive_datetimes do
+    let [
+      year <- integer(1000, 9999),
+      month <- integer(1, 12),
+      day <- integer(1, 31),  # Hand-wavy here
+      hour <- integer(0, 23),
+      minute <- integer(0, 59),
+      second <- integer(0, 59)
     ] do
-      naive_datetime = "#{year}-#{pad2(month)}-#{pad2(day)}T#{pad2(hour)}:#{pad2(minute)}:#{pad2(second)}"
-      :ok == IsoDatetime.isodatetime("#{naive_datetime}+00:00") |> elem(0) &&
-        :ok == IsoDatetime.isodatetime("#{naive_datetime}Z") |> elem(0)
+      "#{year}-#{pad2(month)}-#{pad2(day)}T#{pad2(hour)}:#{pad2(minute)}:#{pad2(second)}"
+    end
+  end
+
+  property "parses a datetime of all valid time with +00:00 ending" do
+    forall naive_datetime <- naive_datetimes() do
+      :ok == IsoDatetime.isodatetime("#{naive_datetime}+00:00") |> elem(0)
+    end
+  end
+
+  property "parses a datetime of all valid time with Z ending" do
+    forall naive_datetime <- naive_datetimes() do
+      :ok == IsoDatetime.isodatetime("#{naive_datetime}Z") |> elem(0)
     end
   end
 end
+
+
+
